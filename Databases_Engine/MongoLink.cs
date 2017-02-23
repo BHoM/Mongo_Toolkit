8db4563a5ae89aedb3c8b474266a97db0ad85bfa
adecler <arnaud.declercq@burohappold.com>
2017-02-23 14:52:36 +0000

    adding timestamp and history

--- Databases_Engine/MongoLink.cs
+++ Databases_Engine/MongoLink.cs
@@ -8,6 +8,7 @@ using BHoM.Global;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using BHoM.Databases;
+using System.Text.RegularExpressions;
 using System.IO;
 
 namespace Mongo_Adapter
@@ -16,12 +17,21 @@ namespace Mongo_Adapter
     {
         private MongoClient m_Client;
         private IMongoCollection<BsonDocument> m_Collection;
+        private IMongoCollection<BsonDocument> m_History;
 
         public MongoLink(string serverLink = "mongodb://localhost:27017", string databaseName = "project", string collectionName = "bhomObjects")
         {
+            if (!serverLink.StartsWith("mongodb://"))
+                serverLink = "mongodb://" + serverLink + ":27017";
+
             m_Client = new MongoClient(serverLink);
             IMongoDatabase database = m_Client.GetDatabase(databaseName);
             m_Collection = database.GetCollection<BsonDocument>(collectionName);
+
+            HistorySize = 20;
+
+            IMongoDatabase hist_Database = m_Client.GetDatabase(databaseName + "_History");
+            m_History = hist_Database.GetCollection<BsonDocument>(collectionName);
         }
 
         /*******************************************/
@@ -51,21 +61,37 @@ namespace Mongo_Adapter
 
         /*******************************************/
 
+        public int HistorySize { get; set; }
+
+        /*******************************************/
+
         public bool Push(IEnumerable<object> objects, string key, List<string> tags = null)
         {
             if (m_Client.Cluster.Description.State == MongoDB.Driver.Core.Clusters.ClusterState.Disconnected)
                 return false;
 
             // Create the bulk query for the object to replace/insert
-                List<WriteModel<BsonDocument>> bulk = new List<WriteModel<BsonDocument>>();
-            bulk.Add(new DeleteManyModel<BsonDocument>(Builders<BsonDocument>.Filter.Eq("__Key__", key)));
+            DateTime timestamp = DateTime.Now; 
+            List<WriteModel<BsonDocument>> bulk = new List<WriteModel<BsonDocument>>();
+            WriteModel<BsonDocument> deletePrevious = new DeleteManyModel<BsonDocument>(Builders<BsonDocument>.Filter.Eq("__Key__", key));
+            bulk.Add(deletePrevious);
             foreach (object obj in objects)
-                bulk.Add(new InsertOneModel<BsonDocument>(ToBson(obj, key)));
+                bulk.Add(new InsertOneModel<BsonDocument>(ToBson(obj, key, timestamp)));
 
             // Send that query
             BulkWriteOptions bulkOptions = new BulkWriteOptions();
             bulkOptions.IsOrdered = true;
             m_Collection.BulkWrite(bulk, bulkOptions);
+
+            // Push in the history database as well
+            bulk.Remove(deletePrevious);
+            List<object> times = Query(new List<string> { "{$group: {_id: \"$__Time__\"}}", "{$sort: {_id: -1}}" });
+            if (times.Count > HistorySize)
+            {
+                bulk.Insert(0, new DeleteManyModel<BsonDocument>(Builders<BsonDocument>.Filter.Lte("__Time__", times[HistorySize])));
+            }
+
+            m_History.BulkWrite(bulk, bulkOptions);
             return true;
         }
 
@@ -111,27 +137,43 @@ namespace Mongo_Adapter
                 return result.Select(x => FromBson(x)).ToList<object>();
         }
 
+        /*******************************************/
+
+        public List<string> GetHistoryTimes()
+        {
+            List<object> times = Query(new List<string> { "{$group: {_id: \"$__Time__\"}}", "{$sort: {_id: -1}}" });
+
+        }
 
         /*******************************************/
         /****  Private Helper Methods           ****/
         /*******************************************/
 
-        private BsonDocument ToBson(object obj, string key)
+        private BsonDocument ToBson(object obj, string key, DateTime timestamp)
         {
-            if (obj is string) return ToBson(obj as string, key);
+            if (obj is string) return ToBson(obj as string, key, timestamp);
             var document = BsonDocument.Parse(BHoM.Base.JSONWriter.Write(obj));  
             if (key != "")
+            {
                 document["__Key__"] = key;
+                document["__Time__"] = timestamp;
+            }
+                
+            
             return document;
         }
 
         /*******************************************/
 
-        private BsonDocument ToBson(string obj, string key)
+        private BsonDocument ToBson(string obj, string key, DateTime timestamp)
         {
             var document = BsonDocument.Parse(obj);
             if (key != "")
+            {
                 document["__Key__"] = key;
+                document["__Time__"] = timestamp;
+            }
+                
             return document;
         }
 
