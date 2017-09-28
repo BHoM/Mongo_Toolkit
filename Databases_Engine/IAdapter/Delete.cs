872dd0baee66e39d5abc481092f2eb8ce0edab12
Eduardo Pignatelli <eduardo.pignatelli@burohappold.com>
2017-09-28 11:25:00 +0100

    namespace format

--- Databases_Engine/IAdapter/Delete.cs
+++ Databases_Engine/IAdapter/Delete.cs
@@ -3,15 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BH.oM.Base;
-using MongoDB.Bson;
 using MongoDB.Driver;
-using BHC = BH.Adapter.Convert;
-using System.Text.RegularExpressions;
-using System.IO;
-using System.Collections;
 using BH.Adapter.Queries;
-using BH.Adapter;
 
 namespace BH.Adapter.Mongo
 {
