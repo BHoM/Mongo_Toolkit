7aa0da807cc8036ec6aa6afd85f9e4c7c4a3d2d2
Emidio <emidio.piermarini@burohappold.com>
2018-01-15 22:01:56 +0800

    Move match query to enginer - #proper

---
+++ Mongo_Engine/Create/MatchQuery.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Engine.Mongo
+{
+    public static partial class Create
+    {
+        public static List<string> MatchQuery(string Key, List<object> Filter)
+        {
+            string projectquery = "";
+            string matchquery = "";
+            string mongolist = "";
+            List<string> aggregatecommand = new List<string>();
+            for (int i = 0; i < Filter.Count; i++)
+            {
+                if (mongolist != "")
+                {
+
+                    mongolist = mongolist + "," + Filter[i];
+                }
+                else
+                { 
+                        mongolist = Filter[i].ToString();
+                }
+            }
+           
+            projectquery = "{$addFields: {isinfilterlist: {$in: [" + "\"$" + Key + "\"," + "[" + mongolist + "]]}}}"; 
+            matchquery= "{$match:{" +"isinfilterlist" + ":true}}";
+            
+            aggregatecommand.Add(projectquery);
+            aggregatecommand.Add(matchquery);
+
+            return aggregatecommand;
+            
+        }
+
+    }
+}
