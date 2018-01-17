0c5435af470314de1e8b90c4d8fd1e2074c4eba9
INaslund <isak.naslund@burohappold.com>
2018-01-17 17:32:29 +0100

    BHoMObject to IObject

--- Mongo_Adapter/CRUD/Push.cs
+++ Mongo_Adapter/CRUD/Push.cs
@@ -14,7 +14,7 @@ namespace BH.Adapter.Mongo
         /**** Public Methods                            ****/
         /***************************************************/
 
-        public override bool Push(IEnumerable<BHoMObject> objects, string tag = "", Dictionary<string, object> config = null)
+        public override bool Push(IEnumerable<IObject> objects, string tag = "", Dictionary<string, object> config = null)
         {
             // Check that the link is still alive
             if (m_Client.Cluster.Description.State == MongoDB.Driver.Core.Clusters.ClusterState.Disconnected)
