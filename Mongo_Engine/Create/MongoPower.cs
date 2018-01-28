137cfaff420d6174b55cf14c52c617bf1d14514c
Emidio <emidio.piermarini@burohappold.com>
2018-01-28 14:17:31 +0800

    Complete WIP PEMDAS Tools
    WIP
    
    exponets
    multiply
    division
    addition
    subtraction
    
    goodbye excel

---
+++ Mongo_Engine/Create/MongoPower.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Engine.Mongo
+{
+    public static partial class Create
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+        public static string MongoPower(object Base, object Exponent)
+        {
+            string aggregatecommand = "";
+            string PowVariableArray = "";
+
+            //process numerator
+            if (Base is double || Base is int)
+            {
+                PowVariableArray = Base.ToString();
+            }
+            else if (Base is string)
+            {
+                if (Base.ToString().StartsWith("{"))  //if you are a mongo expresssion no need for quotes
+                {
+                    PowVariableArray = Base.ToString();
+                }
+                else //if you are just a string, we need to add quotes and a $ to you
+                {
+                    PowVariableArray = "\"$" + Base.ToString() + "\"";
+                }
+            }
+
+            //process denominator
+            if (Exponent is double || Exponent is int)
+            {
+                PowVariableArray = PowVariableArray + "," + Exponent.ToString();
+            }
+            else if (Exponent is string)
+            {
+                if (Exponent.ToString().StartsWith("{"))  //if you are a mongo expresssion no need for quotes
+                {
+                    PowVariableArray = PowVariableArray + "," + Exponent.ToString();
+                }
+                else //if you are just a string, we need to add quotes and a $ to you
+                {
+                    PowVariableArray = PowVariableArray + "," + "\"$" + Exponent.ToString() + "\"";
+                }
+            }
+
+            aggregatecommand = "{$pow: [" + PowVariableArray + "] }";
+
+            return aggregatecommand;
+        }
+
+
+    }
+}
---
+++ Mongo_Engine/Create/MongoSubtraction.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Engine.Mongo
+{
+    public static partial class Create
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+        public static string MongoSubtraction(object A, object B)
+        {
+            string aggregatecommand = "";
+            string SubtractVariableArray = "";
+
+            //process numerator
+            if (A is double || A is int)
+            {
+                SubtractVariableArray = A.ToString();
+            }
+            else if (A is string)
+            {
+                if (A.ToString().StartsWith("{"))  //if you are a mongo expresssion no need for quotes
+                {
+                    SubtractVariableArray = A.ToString();
+                }
+                else //if you are just a string, we need to add quotes and a $ to you
+                {
+                    SubtractVariableArray = "\"$" + A.ToString() + "\"";
+                }
+            }
+
+            //process denominator
+            if (B is double || B is int)
+            {
+                SubtractVariableArray = SubtractVariableArray + "," + B.ToString();
+            }
+            else if (B is string)
+            {
+                if (B.ToString().StartsWith("{"))  //if you are a mongo expresssion no need for quotes
+                {
+                    SubtractVariableArray = SubtractVariableArray + "," + B.ToString();
+                }
+                else //if you are just a string, we need to add quotes and a $ to you
+                {
+                    SubtractVariableArray = SubtractVariableArray + "," + "\"$" + B.ToString() + "\"";
+                }
+            }
+
+            aggregatecommand = "{$subtract: [" + SubtractVariableArray + "] }";
+
+            return aggregatecommand;
+        }
+
+
+    }
+}
