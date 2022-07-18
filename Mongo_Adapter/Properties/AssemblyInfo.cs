bfc36f0130550b8295f975e0b83daa542a4902b9
FraserGreenroyd <fraser.greenroyd@burohappold.com>
2022-07-18 17:38:04 +0100

    Update Assembly Information

--- Mongo_Adapter/Properties/AssemblyInfo.cs
+++ Mongo_Adapter/Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Databases_Engine")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/BHoM/Mongo_Toolkit")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Databases_Engine")]
